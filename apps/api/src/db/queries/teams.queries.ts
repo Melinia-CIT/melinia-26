1f11427260d643309fce59a7b6bf886f70cc17f1
VishalDhanasekaran <2005vishaldhanasekaran@gmail.com>
2025-12-23 00:48:05 +0530

    feat(api): get list of teams for a particular user

--- apps/api/src/db/queries/teams.queries.ts
+++ apps/api/src/db/queries/teams.queries.ts
@@ -28,3 +28,26 @@ export async function createTeam(input: CreateTeam) {
         data: { team_id }
     };
 } 
+
+
+export async function getAllTeamsForUser(userId: string) {
+    try {
+
+        const rows = await sql`
+          SELECT
+              t.id,
+              t.name          AS team_name,
+              t.event_id,
+              e.name          AS event_name
+          FROM team_members  AS tm
+          JOIN teams         AS t  ON t.id  = tm.team_id
+          JOIN events        AS e  ON e.id  = t.event_id
+          WHERE tm.user_id = ${userId}
+          ORDER BY t.created_at;
+        `;
+
+        return { status: true, statusCode: 200, message: 'List of all teams for this user', data: rows };
+    } catch (error) {
+        throw error;
+    }
+}
--- apps/api/src/routes/team.route.ts
+++ apps/api/src/routes/team.route.ts
@@ -1,19 +1,30 @@
 import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
-import { createEventSchema, eventSchema, type CreateTeam, createTeamSchema } from "@packages/shared/dist";
-import { createTeam } from "../db/queries/teams.queries";
+import { type CreateTeam, createTeamSchema } from "@packages/shared/dist";
+import { createTeam, getAllTeamsForUser } from "../db/queries/teams.queries";
 import { sendError, sendSuccess } from "../utils/response";
 
 export const teamRouter = new Hono();
 
-teamRouter.post("/", zValidator("json", createTeamSchema), async (c)=>{
+teamRouter.post("/", zValidator("json", createTeamSchema), async (c) => {
     try {
         const input = c.req.valid('json');
-        const {statusCode, status, data, message} = await createTeam(input);
+        const { statusCode, status, data, message } = await createTeam(input);
 
-        return sendSuccess(c,data,message,status,statusCode);
-    } catch (error:unknown) {
+        return sendSuccess(c, data, message, status, statusCode);
+    } catch (error: unknown) {
         console.error(error);
         return sendError(c);
     }
 })
+
+teamRouter.get("/:user_id", async (c) => {
+    try {
+        const user_id = c.req.param("user_id");
+        const { statusCode, status, data, message } = await getAllTeamsForUser(user_id);
+        return sendSuccess(c, data, message, status, statusCode);
+    } catch (error: unknown) {
+        console.error(error);
+        return sendError(c);
+    }
+});
