b202ddf6b17dce5bdc09b428cd3fd19913344fbd
Krishneshvar <krishjn07@gmail.com>
2025-12-23 19:58:46 +0530

    feat: add landing page skeleton

---
+++ apps/outreach/src/components/Countdown.tsx
@@ -0,0 +1,14 @@
+function Countdown() {
+    return (
+        <>
+            <div
+                className="h-[20%] bg-cover bg-center flex items-center justify-center text-white text-2xl font-bold"
+                style={{ backgroundImage: "url('/countdown-bg.jpg')" }}
+            >
+                Countdown
+            </div>
+        </>
+    );
+}
+
+export default Countdown;
