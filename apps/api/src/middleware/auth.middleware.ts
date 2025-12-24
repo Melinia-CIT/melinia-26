43ea12faa725964a68a6e03364b57b62a883bb4d
Shree Kottes <kottesh.j@gmail.com>
2025-12-25 00:01:57 +0530

    add bindings to auth middleware

--- apps/api/src/middleware/auth.middleware.ts
+++ apps/api/src/middleware/auth.middleware.ts
@@ -2,7 +2,12 @@ import { createMiddleware } from "hono/factory";
 import { HTTPException } from "hono/http-exception";
 import { verifyToken } from "../utils/jwt";
 
-export const authMiddleware = createMiddleware(async (c, next) => {
+type Variables = {
+    user_id: string,
+    role: string
+}
+
+export const authMiddleware = createMiddleware<{ Variables: Variables }>(async (c, next) => {
     const authzHeader = c.req.header("Authorization");
 
     if (!authzHeader?.startsWith("Bearer ")) {
@@ -16,7 +21,7 @@ export const authMiddleware = createMiddleware(async (c, next) => {
     }
 
     try {
-        const { id, role } = await verifyToken(token);
+        const { id, role } = await verifyToken(token) as { id: string, role: string };
         c.set("user_id", id);
         c.set("role", role);
         await next();
