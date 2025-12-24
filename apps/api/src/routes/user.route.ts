028ebf25c541679e8d4a61ea2535cf68c3a161dc
anushm55 <71762231004@cit.edu.in>
2025-12-24 10:02:37 +0530

    feat: add route to update profile

--- apps/api/src/routes/user.route.ts
+++ apps/api/src/routes/user.route.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
 import { profileSchema , createEventSchema} from "@packages/shared/dist";
-import { createProfile, getProfile , checkProfileCompleted, checkCollegeExists,  checkDegreeExists, setProfileCompleted} from "../db/queries";
+import { createProfile, getProfile , checkProfileCompleted, checkCollegeExists,  checkDegreeExists, setProfileCompleted, updateProfile} from "../db/queries";
 import { getUserID } from "../middleware/profile.middleware";
 import { HTTPException } from "hono/http-exception";
 
@@ -40,11 +40,11 @@ user.post("/profile", getUserID, zValidator("json",profileSchema),async (c) => {
 	}
 	if (input["degree"] !== "other"){
 		const degree_exists = await checkDegreeExists(input["degree"])
+		if (!degree_exists){
+		    throw new HTTPException(400, {message: "degree does not exist"})
+		}
 	}
 
-	if (!college_exists){
-	    throw new HTTPException(400, {message: "degree does not exist"})
-	}
 	const profile_result = await createProfile(user_id, input);
 
 	if (profile_result === undefined){
@@ -57,3 +57,37 @@ user.post("/profile", getUserID, zValidator("json",profileSchema),async (c) => {
 		      message:"Profile created successfully"},200);
 
 })
+
+user.put("/profile", getUserID, zValidator("json", profileSchema), async (c) => {
+	const user_id = c.get("user_id");
+	const profile_completed = await checkProfileCompleted(user_id);
+	
+	if (!profile_completed) {
+		throw new HTTPException(404, { message: "Profile does not exist" });
+	}
+	
+	const input = c.req.valid('json');
+	const college_exists = await checkCollegeExists(input["college"]);
+	
+	if (!college_exists) {
+		throw new HTTPException(400, { message: "College does not exist" });
+	}
+	
+	if (input["degree"] !== "other") {
+		const degree_exists = await checkDegreeExists(input["degree"]);
+		if (!degree_exists) {
+			throw new HTTPException(400, { message: "Degree does not exist" });
+		}
+	}
+	
+	const profile_result = await updateProfile(user_id, input);
+	
+	if (profile_result === undefined) {
+		throw new HTTPException(500, { message: "Internal server error" });
+	}
+	
+	return c.json({
+		status: true,
+		message: "Profile updated successfully"
+	}, 200);
+});
