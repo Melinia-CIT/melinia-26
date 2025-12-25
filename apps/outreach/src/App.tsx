3ecbab39714ddb9ffe0b3a5cd97245d6b7d4292b
Krishneshvar <krishjn07@gmail.com>
2025-12-25 23:38:07 +0530

    feat: add events carousel

--- apps/outreach/src/App.tsx
+++ apps/outreach/src/App.tsx
@@ -1,15 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import Home from "./components/Home";
 
-import { useState } from "react";
-import IntroVideo from "./components/IntroVideo";
-
 function App() {
-	const [showIntro, setShowIntro] = useState(true);
-
 	return (
 		<BrowserRouter>
-			{showIntro && <IntroVideo onComplete={() => setShowIntro(false)} />}
 			<Routes>
 				<Route path="/" element={<Home />} />
 			</Routes>
