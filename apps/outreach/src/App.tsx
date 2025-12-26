fb7c53ca0faa632f17efc5eee1ea4018fff9ea86
Shree Kottes <kottesh.j@gmail.com>
2025-12-26 16:21:46 +0530

    add login skeleton page

--- apps/outreach/src/App.tsx
+++ apps/outreach/src/App.tsx
@@ -1,17 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router";
-import Home from "./components/Home";
-
-import { useState } from "react";
-import IntroVideo from "./components/IntroVideo";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
-	const [showIntro, setShowIntro] = useState(true);
-
 	return (
 		<BrowserRouter>
-			{showIntro && <IntroVideo onComplete={() => setShowIntro(false)} />}
 			<Routes>
 				<Route path="/" element={<Home />} />
+				<Route path="/login" element={<Login />} />
 			</Routes>
 		</BrowserRouter>
 	);
