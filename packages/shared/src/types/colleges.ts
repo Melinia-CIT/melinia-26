10fb6214b4febc1e38770c98e2da220c8634a775
Shree Kottes <kottesh.j@gmail.com>
2026-02-22 19:54:24 +0530

    feat: leaderboard wip

--- packages/shared/src/types/colleges.ts
+++ packages/shared/src/types/colleges.ts
@@ -5,7 +5,9 @@ export const getCollegeLeaderboardSchema = z
     .array(z.object({
         college_id: z.number(),
         college_name: z.string(),
-        points: z.coerce.number(),
+        total_points: z.coerce.number(),
+        participant_count: z.coerce.number(),
+        avg_points_per_user: z.coerce.number(),
         rank: z.coerce.number()
     }))
 
